c38a0d4eb823c7298a6168258236eb74f59343d4
Ravi <ravi@thirdi.ai>
2024-02-15 16:19:49 +0000

    Backup.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 const { Storage } = require("@google-cloud/storage");
 const Multer = require('multer');
-// const openAiS = require('openai');
+const openAI = require('openai');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI('AIzaSyCMOEJ4NSJJ6Pe2QhbyQ5SEvub9N8x4hUw');
@@ -13,6 +13,13 @@ const fs = require('fs');
 // ...
 var staticSrc = path.join(__dirname, "/static/");
 router.use(express.static(staticSrc));
+
+const OPENAI_TEMPARATURE = 0.25
+const OPENAI_RESPONSE_FORMAT = "verbose_json"
+
+const openai = new openAI({
+  apiKey: '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',
+})
 // File Reading
 const multer = Multer({
   storage: Multer.memoryStorage(),
@@ -56,6 +63,52 @@ router.post('/upload', multer.single('csvFile'), (req,res) => {
   }
 })
 
+router.post('/runChatGPT', async (req,res) => {
+  fs.readFile(staticSrc + "basePromptnew.txt", 'utf8', async (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const basePrompt  = data;
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {role: "system", content: "You are a data scientist to analyze datasets. Do not make up information that is not in the dataset. For each analysis, provide the exact and definitive answer and do not provide code or instructions to do the analysis on other platforms"},
+        { role: "user", content: basePrompt }
+      ],
+      temperature: OPENAI_TEMPARATURE,
+      model: "gpt-4-0125-preview",
+    });
+      
+    console.log("First Level");
+    console.log(completion.choices[0]);
+    fs.readFile(staticSrc + "schema2.txt", 'utf8', async (err1, data1) => {
+      if (err1) {
+        console.error(err1);
+        return;
+      }
+      const newR = await openai.chat.completions.create({
+        messages: [
+          { role: "user", content: "Schema format is COLUMN_NAME:data_type \n" + data1 }
+        ],
+        temperature: OPENAI_TEMPARATURE,
+        model: "gpt-4-0125-preview",
+      });
+      console.log("Second Level");
+      console.log(newR.choices[0]);
+      const newR1 = await openai.chat.completions.create({
+        messages: [
+          { role: "user", content: csvData }
+        ],
+        temperature: OPENAI_TEMPARATURE,
+        model: "gpt-4-0125-preview",
+      });
+
+      console.log("Final");
+      console.log(newR1.choices[0].message.content);
+      res.status(200).send(newR1.choices[0].message.content);
+    });
+  });
+});
 
 router.post('/runChat', async (req,res) => {
   fs.readFile(staticSrc + "basePrompt.txt", 'utf8', async (err, data) => {
