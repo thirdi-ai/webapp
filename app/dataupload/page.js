c00f8c5d0bf60b27d7ec33cacaaf4c23ab599bac
nisha-kum <nishakumari@thirdi.ai>
2024-03-06 11:53:34 +0530

    created modalcomponent

--- app/dataupload/page.js
+++ app/dataupload/page.js
@@ -9,9 +9,11 @@ import DataTable from "@/components/DataTable";
 import asset11 from "@/assets/asset11.svg";
 import { Connectors } from "@/Constants/constant";
 import ConnectorsCard from "@/components/ConnectorsCard";
+import ModalComponent from "@/components/ModalComponent";
 
 export default function DataUpload() {
   //  const router = useRouter();
+  const [showModal, setShowModal] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
   const [displayFileName, setDisplayFileName] = useState(false);
   const [showLoader, setShowLoader] = useState(false);
@@ -40,7 +42,6 @@ export default function DataUpload() {
     }
     setCsvData(parsedData);
   };
-
   const handleButtonClick = () => {
     setShowBtn(true);
   };
@@ -71,47 +72,57 @@ export default function DataUpload() {
   };
 
   return (
-    <main className="data-upload w-full relative flex flex-col p-[30px] border-x border-grey overflow-y-scroll custom-scrollbar">
-      <span className="text-base text-grey-800 mb-1 font-[500]">
-        22 Feb, 2024
-      </span>
-      <h1 className="text-3xl text-black-400 font-[500]">Integrations</h1>
-      <div className="w-full min-h-[183px] relative mt-[40px] font-[500]">
-        <Card>
-          <div className="w-full cursor-pointer h-full flex place-items-center relative gap-7">
-            <div className="card-image min-w-[101px] min-h-[97.84px] bg-white-400 relative flex rounded-[18px] ">
-              <Image
-                src={asset11}
-                alt="csv file"
-                width={60}
-                height={60}
-                className="object-contain mx-auto"
-              />
-            </div>
-            <div className="card-text w-full flex flex-col gap-2 relative">
-              <h3 className="text-base text-black-400">CSV Data Connector</h3>
-              <p className="text-[12px] leading-[14.64px] text-black-600 w-[260px]">
-                Maximize ROI across your campaigns by using a portfolio strategy
-                with a shared budget
-              </p>
-              <div className="flex-center w-full relative">
-                <p className="text-[12px] leading-[14.64px] text-grey-400 w-[380px]">
-                  Recommended because you have campaigns that share a bid
-                  strategy but have separate budgets
+    <main className="data-upload w-full flex flex-col z-30 p-[30px] pr-0 border-x border-grey max-h-screen">
+      <div className="mr-[30px]">
+        <span className="text-base text-grey-800 mb-1 font-[500]">
+          22 Feb, 2024
+        </span>
+        <h1 className="text-3xl text-black-400 font-[500]">Integrations</h1>
+        <div className="w-full min-h-[183px] relative mt-[40px] font-[500]">
+          <Card>
+            <div className="w-full cursor-pointer h-full flex place-items-center relative gap-7">
+              <div className="card-image min-w-[101px] min-h-[97.84px] bg-white-400 relative flex rounded-[18px] ">
+                <Image
+                  src={asset11}
+                  alt="csv file"
+                  width={60}
+                  height={60}
+                  className="object-contain mx-auto"
+                />
+              </div>
+              <div className="card-text w-full flex flex-col gap-2 relative">
+                <h3 className="text-base text-black-400">CSV data connector</h3>
+                <p className="text-[12px] leading-[14.64px] text-black-600 w-[260px]">
+                  Maximize ROI across your campaigns by using a portfolio
+                  strategy with a shared budget
                 </p>
-                <button className="btn w-[168px]">Upload</button>
+                <div className="flex-center w-full relative">
+                  <p className="text-[12px] leading-[14.64px] text-grey-400 w-[380px]">
+                    Recommended because you have campaigns that share a bid
+                    strategy but have separate budgets
+                  </p>
+                  <button
+                    className="btn w-[168px]"
+                    onClick={()=>{setShowModal(true)}}
+                  >
+                    Upload
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        </Card>
+          </Card>
+        </div>
+        {showModal && <ModalComponent onBtnClick={()=>{setShowModal(false)}} />}
       </div>
-      <div className="mt-8 ">
+      <div className="mt-8 mr-3">
         <h2 className="text-base text-black-400 font-semibold">Connectors</h2>
-        <div className="flex flex-col gap-5 mt-9 h-[430px] pr-2 overflow-y-scroll custom-scrollbar-2">
+        <div className="flex flex-col gap-5 mt-9 h-[430px] pr-3 overflow-y-scroll custom-scrollbar-2">
           {Connectors.map((connector, index) => {
-            return <div key={index}>
-              <ConnectorsCard {...connector}/>
-            </div>;
+            return (
+              <div key={index}>
+                <ConnectorsCard {...connector} />
+              </div>
+            );
           })}
         </div>
       </div>
---
+++ components/ModalComponent.js
@@ -0,0 +1,99 @@
+import Image from "next/image";
+import asset19 from "@/assets/asset19.svg";
+import asset20 from "@/assets/asset20.svg";
+import asset21 from "@/assets/asset21.svg";
+import asset22 from "@/assets/asset22.svg";
+import Card from "./Card";
+import Link from "next/link";
+
+export default function ModalComponent({ onBtnClick }) {
+  return (
+    <div className="modal-overlay w-[100vw] h-[100vh] overflow-hidden absolute z-50 top-0 left-0 bg-[#0217238A] bg-opacity-[54%] flex justify-center items-center">
+      <div className="modal-wrapper w-[1090px] h-[546px] relative bg-white rounded-md">
+        <div className="modal-content font-[500] flex flex-col h-full relative overflow-hidden">
+          <div className="modal-header p-5 pl-[30px] pb-4 border-b border-grey-600">
+            <div className="flex-center">
+              <h1 className="text-[17px] leading-[20.42px] text-grey-500">
+                New Data Source
+              </h1>
+              <button
+                className="p-1 bg-white-400 rounded-[4px]"
+                onClick={onBtnClick}
+              >
+                <Image src={asset19} width={12} height={12} />
+              </button>
+            </div>
+            <div className="text-[13px] flex gap-6 mt-4">
+              <div className="flex place-items-center border border-blue-600 p-1 pr-3 gap-2 rounded-[4px] text-blue-600">
+                <div className="w-[26px] h-[26px] flex justify-center items-center bg-blue-600 bg-opacity-10 rounded-[4px]">
+                  1
+                </div>
+                Upload File
+              </div>
+              <div className="flex place-items-center p-1 pr-3 gap-2 rounded-[4px] text-grey-400">
+                <div className="w-[26px] h-[26px] flex justify-center items-center  border border-grey-600 bg-opacity-10 rounded-[4px]">
+                  2
+                </div>
+                Data
+              </div>
+            </div>
+          </div>
+          <div className="modal-grid flex h-full relative">
+            <div className="w-2/6 border-r border-grey-600 h-full flex-center flex-col relative">
+              <div className="px-5" >
+                <div className="relative w-full  pt-6 pb-3 flex place-items-center gap-3 border-b border-grey-600">
+                  <Image src={asset21} width={50} />
+                  <span>CSV </span>
+                </div>
+                <div className="text-sm  text-gray-light pt-3">
+                  See <Link href="/" className="text-blue-600">our documentation</Link> for more
+                  information on how to format your CSV file.
+                </div>
+              </div>
+              <div className="h-[54px] relative border-t border-grey-600 w-full text-sm px-5 flex-center">
+                <button>Cancel</button>
+                <button className="flex place-items-center bg-grey text-white px-3 rounded-sm py-1"><Image src={asset22} width={14} height={12} className="mr-1"/>Next</button>
+              </div>
+            </div>
+            <div className="w-4/6 h-full relative p-7 bg-white-smoke">
+              <p className="mb-3 text-sm text-grey-500">Upload CSV File</p>
+              <div className="w-full h-[340px] border border-grey-600 p-2  relative rounded-md bg-white">
+                <div className="w-full h-full rounded-md border border-dashed border-white-500 text-sm flex justify-center items-center flex-col">
+                  <label
+                    htmlFor="fileInput"
+                    className="w-full h-full rounded-md border border-dashed border-white-500 text-sm flex justify-center items-center flex-col"
+                  >
+                    <Image
+                      src={asset20}
+                      width={20}
+                      height={20}
+                      className="mx-auto pb-2"
+                    />
+                    <div className="text-grey-500 ">
+                      Drag and drop or
+                      <span className="text-blue-600 underline font-[500] cursor-pointer mx-1">
+                        browse your files
+                      </span>
+                      to upload
+                    </div>
+                    <span className="text-xs mt-1 text-gray-light">
+                      .csv or .xlsx 1GB max
+                    </span>
+                    <input
+                      type="file"
+                      id="fileInput"
+                      name="csvFile"
+                      accept=".csv, .xls, .xlsx"
+                      className="inputfile hidden"
+                      // onChange={handleFileChange}
+                    />
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
